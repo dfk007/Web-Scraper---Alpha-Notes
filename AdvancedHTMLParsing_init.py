322bb0cf52ea1d6eedfff238e298d12bfbaed860
dfk007 <df89@student.london.ac.uk>
2021-12-25 23:34:01 +0500

    Add files via upload

---
+++ AdvancedHTMLParsing_init.py
@@ -0,0 +1,3 @@
+# We'll look at Parsing Complicated HTML Pages in order to extract only the information we need.
+# Avoid layering scraping techniques recklessly, as it can lead to code i.e. difficult
+# to debug, fragile, or both. 
